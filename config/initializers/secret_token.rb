e0bd7567eb1a96605920080a57c8a747da7ba613
Jason Gilman <jason.gilman@gmail.com>
2010-12-11 10:54:57 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MessageCloud::Application.config.secret_token = '6bdad94de239a5f357c2eb4085a10b5f3d791864486f60cf1d031e63c35b369a8f6fe648e49870facc618b75b895482f6e6fc724c819cf9113f5a3f76f586a02'
