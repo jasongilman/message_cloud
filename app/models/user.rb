efbf8dae9e48fa2b0b5472eed979ec05af01e891
Jason Gilman <jason.gilman@gmail.com>
2010-12-11 15:35:22 -0500

    Added username to User
    Added validations for username.
    Updated pages allowing user to be created or edited to also use the username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
+  
+  validates_length_of :username, :minimum => 4, :maximum=>14
+  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :username, :password, :password_confirmation, :remember_me
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "Username should be at least 4 characters long" do 
+    u = User.new(:username=>"abc")
+    assert !u.valid?
+    assert_equal ["is too short (minimum is 4 characters)"], u.errors[:username]
   end
+  
+  test "Username should not be greater than 14 characters long" do 
+    u = User.new(:username=>"123456789012345")
+    assert !u.valid?
+    assert_equal ["is too long (maximum is 14 characters)"], u.errors[:username]
+  end
+
+  test "Username is required" do 
+    u = User.new(:username=>nil)
+    assert !u.valid?
+    assert_equal ["is too short (minimum is 4 characters)"], u.errors[:username]
+  end
+  
+  
 end
